29c5a979059c53bf46e683a5d42b98e62186038c
kunjilaldalsevwale-prog <kunjilaldalsevwale@gmail.com>
2026-05-22 22:46:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const fs = require('fs');
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
-app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
+app.use(express.static(path.join(__dirname, 'frontend/dist')))  app.get('*', (req, res) => {   res.sendFile(path.join(__dirname, 'frontend/dist/index.html')) });
 
 const uploadsDir = path.join(__dirname, 'uploads');
 if (!fs.existsSync(uploadsDir)) fs.mkdirSync(uploadsDir);
