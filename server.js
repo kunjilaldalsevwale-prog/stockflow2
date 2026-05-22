5ce62535bcf0e0a6793fb5bfa411b4e540a1ff57
kunjilaldalsevwale-prog <kunjilaldalsevwale@gmail.com>
2026-05-22 22:34:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1008,4 +1008,9 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
+app.use(express.static(path.join(__dirname, 'frontend', 'dist')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'))
+})
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
